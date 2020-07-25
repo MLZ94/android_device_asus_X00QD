b17568fff083d3cc52a077d83fc38153a9318e59
Jackeagle <jackeagle102@gmail.com>
2020-07-25 19:02:30 +0200

    X00QD: Downgrade SoundTrigger to 2.1
    - Asus speaker nxp amp does not work on 2.2
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I5656b0219eb5c13f2453b9c474770a5d77df9c21

--- device.mk
+++ device.mk
@@ -81,7 +81,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.1-impl \
     android.hardware.audio.common@2.0-util \
     android.hardware.audio.common@5.0-util \
     audio.a2dp.default \
