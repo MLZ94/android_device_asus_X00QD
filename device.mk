e07c2adb4d20f7bb4a59220ca9d9172d1f3bc6be
MLZ94 <zugutoroberto@gmail.com>
2021-02-25 20:56:21 +0100

    X00QD: use prebuilt modulemetadata

--- device.mk
+++ device.mk
@@ -574,6 +574,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# Prebuimt modulemetadata
+PRODUCT_PACKAGES += \
+    com.google.android.modulemetadata
+    
 # ZenParts
 PRODUCT_PACKAGES += \
     ZenParts
