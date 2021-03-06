c41bbf655c0b127d708333c42baf6cdf6f022b6b
jefinhodatnt <jefiinhorelikia@gmail.com>
2021-03-05 23:50:53 -0300

    X00QD: Add Vanced Manager
    Signed-off-by: jefinhodatnt <jefiinhorelikia@gmail.com>

--- device.mk
+++ device.mk
@@ -570,6 +570,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     GCamGo \
     GalleryGo \
-    Gboard
+    Gboard \
+    VancedManager
 
 $(call inherit-product, vendor/asus/X00QD/X00QD-vendor.mk)
