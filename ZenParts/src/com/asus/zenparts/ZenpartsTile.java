be77df65f7fe1c6d7004f8f2e4ad9e83d8da2356
MLZ94 <zugutoroberto@gmail.com>
2021-03-07 02:42:22 +0100

    Revert "[WIP]X00QD: add more import for team menu"
    This reverts commit f05281cc9a29a270f3888ecd86b87798dcda361c.
    
    Revert "[WIP]X00QD: add team menu to zenparts"
    
    This reverts commit e8b01acb977ef0f95cdeb882f431a7017a966288.

--- ZenParts/src/com/asus/zenparts/ZenpartsTile.java
+++ ZenParts/src/com/asus/zenparts/ZenpartsTile.java
@@ -4,30 +4,6 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.service.quicksettings.TileService;
 
-import com.android.internal.logging.nano.MetricsProto;
-import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
-
-import android.app.ActionBar;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.TypedValue;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import androidx.viewpager.widget.ViewPager;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.fragment.app.FragmentPagerAdapter;
-
-import com.asus.zenparts.fragments.team.TeamActivity;
-
 import com.asus.zenparts.DeviceSettings;
 import com.asus.zenparts.DeviceSettingsActivity;
 
@@ -44,18 +20,5 @@ public class ZenpartsTile extends TileService {
             Intent intent = new Intent(this, DeviceSettings.class);
             startActivityAndCollapse(intent);
         }
-            @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-		menu.add(0, 0, 0, R.string.dialog_team_title);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-	    if (item.getItemId() == 0) {
-		    Intent intent = new Intent(mContext, TeamActivity.class);
-		    mContext.startActivity(intent);
-		    return true;
-	    }
-	    return false;
     }
 }
