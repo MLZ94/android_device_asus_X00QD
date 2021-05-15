5d806a0c16b75f0acc8cbf6ca74bdbe95c56cb59
henricaodopao <allricknation@gmail.com>
2021-05-15 13:18:53 +0000

    X00QD: uprev display config to 1.9

--- device.mk
+++ device.mk
@@ -194,7 +194,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    vendor.display.config@1.3 \
+    vendor.display.config@1.9 \
     copybit.sdm660 \
     gralloc.sdm660 \
     hwcomposer.sdm660 \
