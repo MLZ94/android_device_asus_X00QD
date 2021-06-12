64823f26a960257aa2a894e257d9631e380f2e01
MLZ94 <zugutoroberto@gmail.com>
2021-06-13 00:31:25 +0200

    X00QD: update fingerprint

--- init/init_X00QD.cpp
+++ init/init_X00QD.cpp
@@ -107,8 +107,8 @@ void vendor_check_variant()
     product_name = "ASUS_X00QD";
     product_device = "ASUS_X00QD";
     product_model = "ASUS_X00QD";
-    build_fingerprint = "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys";
-    build_description = "redfin-user 11 RQ2A.210505.003 7255357 release-keys";
+    build_fingerprint = "google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys";
+    build_description = "redfin-user 11 RQ3A.210605.005 7349499 release-keys";
 
     // Override props based on values set
     property_override("ro.build.description", build_description);
