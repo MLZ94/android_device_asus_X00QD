0013009ed56835e2edf65693cfdcf752761a4ada
MLZ94 <zugutoroberto@gmail.com>
2021-06-20 02:51:31 +0200

    Revert "X00QD: use prebuilt gcam back"
    This reverts commit bb334d29610f5630cb4604ca8b8850411095ed91.

--- device.mk
+++ device.mk
@@ -397,13 +397,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
 
-# Prebuilts
-PRODUCT_PACKAGES += \
-    GoogleCamera
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilts/system/etc/permissions/com.google.android.GoogleCameraEng.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.android.GoogleCameraEng.xml
-    
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
