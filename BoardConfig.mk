fb79931c655ec18f1e29ba5a5d1d605999cada5f
henricaodopao1 <dragonsxbr@gmail.com>
2021-09-18 14:26:01 +0000

    X00QD: move to .prop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,6 +166,9 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/bus/i2c/devices/4-0038/dclick_mode"
 # Properties
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
