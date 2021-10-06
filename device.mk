89f986bb11e5021863e1fdf91502d883ad0e4142
maitreya29 <maitreyapatni30@gmail.com>
2021-10-06 13:00:36 +0530

    X00QD: Build android.hardware.radio.deprecated

--- device.mk
+++ device.mk
@@ -529,6 +529,8 @@ PRODUCT_PACKAGES += \
 
 # Radio
 PRODUCT_PACKAGES += \
+    android.hardware.radio.deprecated@1.0 \
+    android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.radio@1.5 \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2 \
