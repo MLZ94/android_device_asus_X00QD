aacf1bd3cff1ad4a8f831794e31221735a485986
henricaodopao1 <dragonsxbr@gmail.com>
2021-10-08 16:33:47 -0300

    X00QD: dont build bluetooth 1.1 * build 1.0 normal variant

--- device.mk
+++ device.mk
@@ -146,8 +146,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0 \
     android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio@2.0-impl \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
