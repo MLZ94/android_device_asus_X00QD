6ee6cf0761cf40f96e9c9b81259e2eb607caffc6
henricaodopao1 <dragonsxbr@gmail.com>
2021-10-08 12:03:51 -0300

    X00QD: build more needed hals

--- device.mk
+++ device.mk
@@ -153,6 +153,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
+    android.frameworks.displayservice@1.0.vendor \
     android.hardware.camera.common@1.0 \
     android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4-impl \
@@ -202,7 +203,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.3-service.clearkey \
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.0.vendor \
+    android.hardware.drm@1.1.vendor \
+    android.hardware.drm@1.2.vendor \
+    android.hardware.drm@1.3.vendor
 
 # Exclude TOF sensor from InputManager
 PRODUCT_COPY_FILES += \
