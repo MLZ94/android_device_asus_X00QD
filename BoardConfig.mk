28b490d56ff56d86524f3e27ba7a0d56a41f9937
henricaodopao1 <dragonsxbr@gmail.com>
2021-10-08 08:49:49 -0300

    [DMN]X00QD: Ignore neverallows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -191,6 +191,7 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 VENDOR_SECURITY_PATCH := 2020-05-05
 
 # SELinux
+SELINUX_IGNORE_NEVERALLOWS := true
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
