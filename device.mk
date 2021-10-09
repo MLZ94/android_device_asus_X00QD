cacccdf022534576aba3fe4fdb30a0cc38750a32
henricaodopao1 <dragonsxbr@gmail.com>
2021-10-09 13:39:40 -0300

    X00QD: Downgrade radio to 1.4 *fixes ril

--- device.mk
+++ device.mk
@@ -552,8 +552,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0 \
     android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.radio@1.5 \
-    android.hardware.radio@1.5.vendor \
+    android.hardware.radio@1.4 \
+    android.hardware.radio@1.4.vendor \
     android.hardware.radio.config@1.2 \
     android.hardware.radio.config@1.2.vendor
 
