9e8e3b56a32da3403701af83f2f6405c807a3f0e
MLZ94 <zugutoroberto@gmail.com>
2022-11-18 10:28:09 +0100

    X00QD: drop Graphene camera
    This reverts commit 5db7dd924d0084cffefaab22362c090899cca2b4.

--- device.mk
+++ device.mk
@@ -112,9 +112,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0
 
-# GrapheneCamera
-PRODUCT_PACKAGES += GrapheneCamera
-
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
