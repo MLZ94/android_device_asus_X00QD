f5b2c9fa70773f322c9b1533a9c282ab3d77dffa
MLZ94 <zugutoroberto@gmail.com>
2022-11-19 19:40:28 +0100

    X00QD: overlay: remove overlay-aosp
    * Also relocate overlays to their correct location
    
    Signed-off-by: MLZ94 <zugutoroberto@gmail.com>

--- device.mk
+++ device.mk
@@ -308,8 +308,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
