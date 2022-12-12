83702dd3d0d11a467fbe444637ed0cdaf4cddbf3
Albert I <kras@raphielgang.org>
2022-12-12 12:22:58 -0500

    X00QD: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc
    Signed-off-by: MLZ94 <zugutoroberto@gmail.com>

--- device.mk
+++ device.mk
@@ -442,6 +442,9 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel
 
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
