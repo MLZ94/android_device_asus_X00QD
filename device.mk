a253bffe018d97b099c0edb0a38431b525c3f7c0
OdSazib <odsazib@gmail.com>
2023-01-01 20:04:30 +0100

    X00QD: Uprev camera.device to 3.5
    * and camera.provider to 2.6
    
    Signed-off-by: MLZ94 <zugutoroberto@gmail.com>

--- device.mk
+++ device.mk
@@ -104,10 +104,10 @@ TARGET_SCREEN_WIDTH := 1080
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.common@1.0 \
-    android.hardware.camera.device@3.4 \
+    android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5 \
+    android.hardware.camera.provider@2.6 \
     vendor.qti.hardware.camera.device@1.0
 
 # Cgroup and task_profiles
